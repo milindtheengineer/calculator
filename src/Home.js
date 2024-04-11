a1e0f1849fd75bfc3c741aef86f106a9b4c6b261
Milind Juttiga <milindjuttiga@Milinds-Air.attlocal.net>
2024-04-11 12:40:54 -0700

    more css

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,31 @@
 import { useState } from "react";
 import "./form.css";
+import "./output.css";
 
 const Home = () => {
-  const [houseCost, setHouseCost] = useState(0);
-  const [downPayment, setDownpayment] = useState(0);
-  const [mortgagePercentage, setMortgagePercentage] = useState(0);
-  const [hoa, setHOA] = useState(0);
-  const [taxPercentage, setTaxPercentage] = useState(0);
-  const [houseInsurancePerMonth, setHouseInsurancePerMonth] = useState(0);
-  const [mortgageYears, setMortgageYears] = useState(0);
-  const [investableAmountPerMonth, setInvestableAmountPerMonth] = useState(0);
-  const [currentRent, setCurrentRent] = useState(0);
-  const [investmentInterest, setInvestmentInterest] = useState(0);
+  const [houseCost, setHouseCost] = useState("");
+  const [downPayment, setDownpayment] = useState("");
+  const [mortgagePercentage, setMortgagePercentage] = useState("");
+  const [hoa, setHOA] = useState("");
+  const [taxPercentage, setTaxPercentage] = useState("");
+  const [houseInsurancePerMonth, setHouseInsurancePerMonth] = useState("");
+  const [mortgageYears, setMortgageYears] = useState("");
+  const [investableAmountPerMonth, setInvestableAmountPerMonth] = useState("");
+  const [currentRent, setCurrentRent] = useState("");
+  const [investmentInterest, setInvestmentInterest] = useState("");
   ////
-  const [mortgagePerMonth, setMortgagePerMonth] = useState(0);
-  const [totalCostPerMonth, setTotalCostPerMonth] = useState(0);
-  const [yearsToFinishMortgage, setYearsToFinishMortgage] = useState(0);
+  const [mortgagePerMonth, setMortgagePerMonth] = useState("");
+  const [totalCostPerMonth, setTotalCostPerMonth] = useState("");
+  const [yearsToFinishMortgage, setYearsToFinishMortgage] = useState("");
   const [extraAmountToPayEveryMonth, setExtraAmountToPayEveryMonth] =
-    useState(0);
+    useState("");
   const [totalExtraAmountToPayEveryMonth, setTotalExtraAmountToPayEveryMonth] =
-    useState(0);
+    useState("");
   const [investmentValueWithNoHouse, setInvestmentValueWithNoHouse] =
-    useState(0);
-  const [investmentValueWithHouse, setInvestmentValueWithHouse] = useState(0);
-
+    useState("");
+  const [investmentValueWithHouse, setInvestmentValueWithHouse] = useState("");
+  const [desiredHouseValueInFuture, setDesiredHouseValueInFuture] =
+    useState("");
   const calculateCompoundInterest = (
     principal,
     annualInterestRate,
@@ -69,6 +71,10 @@ const Home = () => {
     return newMonthlyPayment;
   };
 
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const mortgage = calculateMortgagePerMonth(
@@ -110,114 +116,133 @@ const Home = () => {
       parseInt(yearsToFinishMortgage)
     );
     setInvestmentValueWithHouse(valueWithHouse);
+    setDesiredHouseValueInFuture(valueWithNoHouse - valueWithHouse);
   };
 
   return (
     <div className="home">
-      <form onSubmit={handleSubmit}>
-        <label>Home cost</label>
-        <input
-          type="number"
-          required
-          value={houseCost}
-          onChange={(e) => setHouseCost(e.target.value)}
-          id="feedback-phone"
-        />
-        <label>Down payment</label>
-        <input
-          type="number"
-          required
-          value={downPayment}
-          onChange={(e) => setDownpayment(e.target.value)}
-          id="feedback-phone"
-        />
-        <label>Mortgage percentage</label>
-        <input
-          type="number"
-          required
-          value={mortgagePercentage}
-          onChange={(e) => setMortgagePercentage(e.target.value)}
-          id="feedback-phone"
-        />
-        <label>Mortgage years</label>
-        <input
-          type="number"
-          required
-          value={mortgageYears}
-          onChange={(e) => setMortgageYears(e.target.value)}
-          id="feedback-phone"
-        />
-        <label>HOA</label>
-        <input
-          type="number"
-          required
-          value={hoa}
-          onChange={(e) => setHOA(e.target.value)}
-          id="feedback-phone"
-        />
-        <label>Tax percentage</label>
-        <input
-          type="number"
-          required
-          value={taxPercentage}
-          onChange={(e) => setTaxPercentage(e.target.value)}
-          id="feedback-phone"
-        />
-        <label>House Insurance</label>
-        <input
-          type="number"
-          required
-          value={houseInsurancePerMonth}
-          onChange={(e) => setHouseInsurancePerMonth(e.target.value)}
-          id="feedback-phone"
-        />
-        <label>Years to finish mortgage</label>
-        <input
-          type="number"
-          required
-          value={yearsToFinishMortgage}
-          onChange={(e) => setYearsToFinishMortgage(e.target.value)}
-          id="feedback-phone"
-        />
-        <label>Investable amount per month</label>
-        <input
-          type="number"
-          required
-          value={investableAmountPerMonth}
-          onChange={(e) => setInvestableAmountPerMonth(e.target.value)}
-          id="feedback-phone"
-        />
-        <label>Investment interest percentage</label>
-        <input
-          type="number"
-          required
-          value={investmentInterest}
-          onChange={(e) => setInvestmentInterest(e.target.value)}
-          id="feedback-phone"
-        />
-        <label>Rent per month</label>
-        <input
-          type="number"
-          required
-          value={currentRent}
-          onChange={(e) => setCurrentRent(e.target.value)}
-          id="feedback-phone"
-        />
-        <button>Calculate</button>
-      </form>
+      <div className="home-form">
+        <form onSubmit={handleSubmit}>
+          <label>House cost</label>
+          <input
+            type="number"
+            required
+            value={houseCost}
+            onChange={(e) => setHouseCost(e.target.value)}
+            id="feedback-phone"
+          />
+          <label>Down payment</label>
+          <input
+            type="number"
+            required
+            value={downPayment}
+            onChange={(e) => setDownpayment(e.target.value)}
+            id="feedback-phone"
+          />
+          <label>Mortgage percentage</label>
+          <input
+            type="number"
+            required
+            value={mortgagePercentage}
+            onChange={(e) => setMortgagePercentage(e.target.value)}
+            id="feedback-phone"
+          />
+          <label>Loan term</label>
+          <input
+            type="number"
+            required
+            value={mortgageYears}
+            onChange={(e) => setMortgageYears(e.target.value)}
+            id="feedback-phone"
+          />
+          <label>HOA per month</label>
+          <input
+            type="number"
+            required
+            value={hoa}
+            onChange={(e) => setHOA(e.target.value)}
+            id="feedback-phone"
+          />
+          <label>Tax percentage per year</label>
+          <input
+            type="number"
+            required
+            value={taxPercentage}
+            onChange={(e) => setTaxPercentage(e.target.value)}
+            id="feedback-phone"
+          />
+          <label>House Insurance per month</label>
+          <input
+            type="number"
+            required
+            value={houseInsurancePerMonth}
+            onChange={(e) => setHouseInsurancePerMonth(e.target.value)}
+            id="feedback-phone"
+          />
+          <label>Desired years to finish mortgage</label>
+          <input
+            type="number"
+            required
+            value={yearsToFinishMortgage}
+            onChange={(e) => setYearsToFinishMortgage(e.target.value)}
+            id="feedback-phone"
+          />
+          <label>Investable amount per month</label>
+          <input
+            type="number"
+            required
+            value={investableAmountPerMonth}
+            onChange={(e) => setInvestableAmountPerMonth(e.target.value)}
+            id="feedback-phone"
+          />
+          <label>Investment interest percentage per annum</label>
+          <input
+            type="number"
+            required
+            value={investmentInterest}
+            onChange={(e) => setInvestmentInterest(e.target.value)}
+            id="feedback-phone"
+          />
+          <label>Rent per month</label>
+          <input
+            type="number"
+            required
+            value={currentRent}
+            onChange={(e) => setCurrentRent(e.target.value)}
+            id="feedback-phone"
+          />
+          <button>Calculate</button>
+        </form>
+      </div>
       <div className="output">
-        <p>Mortgage per month</p>
-        <p>{mortgagePerMonth}</p>
-        <p>Total Payment Per Month</p>
-        <p>{totalCostPerMonth}</p>
-        <p>Extra Payment Per Month</p>
-        <p>{extraAmountToPayEveryMonth}</p>
-        <p>Total Extra Payment Per Month</p>
-        <p>{totalExtraAmountToPayEveryMonth}</p>
-        <p>Investment value without house</p>
-        <p>{investmentValueWithNoHouse}</p>
-        <p>Investment value with house</p>
-        <p>{investmentValueWithHouse}</p>
+        <div className="value-cell">
+          <h3>Mortgage per month</h3>
+          <p>{numberWithCommas(Math.ceil(mortgagePerMonth))}</p>
+        </div>
+        <div className="value-cell">
+          <h3>Total Payment Per Month</h3>
+          <p>{numberWithCommas(Math.ceil(totalCostPerMonth))}</p>
+        </div>
+        <div className="value-cell">
+          <h3>Extra Payment Per Month</h3>
+          <p>{numberWithCommas(Math.ceil(extraAmountToPayEveryMonth))}</p>
+        </div>
+        <div className="value-cell">
+          <h3>Total Extra Payment Per Month</h3>
+          <p>{numberWithCommas(Math.ceil(totalExtraAmountToPayEveryMonth))}</p>
+        </div>
+        <div className="value-cell">
+          <h3>Investment value without house</h3>
+          <p>{numberWithCommas(Math.ceil(investmentValueWithNoHouse))}</p>
+        </div>
+        <div className="value-cell">
+          <h3>Investment value with house</h3>
+          <p>{numberWithCommas(Math.ceil(investmentValueWithHouse))}</p>
+        </div>
+        <div className="value-cell">
+          <h3>Desired House value in future</h3>
+          <p>{numberWithCommas(Math.ceil(desiredHouseValueInFuture))}</p>
+        </div>
       </div>
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Calculator</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create">About</Link>
+      </div>
     </nav>
   );
 };
