8da593a79ebb96f80666edcb685c2f02dfe3f2e6
Milind Juttiga <milindjuttiga@Milinds-Air.attlocal.net>
2024-04-11 13:00:25 -0700

    some changes to test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./index.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
+import About from "./About";
 import NotFound from "./NotFound";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
             {/* Define other routes here */}
             <Route path="*" element={<NotFound />} />
           </Routes>
