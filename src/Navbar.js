e0bb534a5148d79fa5d706b73c063a66f78e21be
Milind Juttiga <milindjuttiga@Milinds-Air.attlocal.net>
2024-04-15 19:44:47 -0700

    change wording

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Calculator</h1>
+      <h1>Mortgage/Investment Calculator</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
